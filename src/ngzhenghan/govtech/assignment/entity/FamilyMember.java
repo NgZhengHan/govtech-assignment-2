59617af3842be309fe277406a9693259bdba6d53
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-14 04:40:31 +0800

    Added class FamilyMember
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

---
+++ src/ngzhenghan/govtech/assignment/entity/FamilyMember.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ngzhenghan.govtech.assignment.entity;
+
+/**
+ * @author Ng Zheng Han
+ * 
+ * Class to define a family member
+ *
+ */
+public class FamilyMember {
+
+	private static final String UNDEFINED_STRING = "Undefined";
+	
+	private String name = UNDEFINED_STRING;
+	
+}
