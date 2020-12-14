7e5f8dc94a5f0b0078a2fed8cf19f32dd53931a6
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-14 22:08:39 +0800

    Added utility class HibernateUtility for Hibernate
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

---
+++ src/ngzhenghan/govtech/assignment/hibernate/HibernateUtility.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package ngzhenghan.govtech.assignment.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * @author Ng Zheng Han
+ *
+ * Convenience class to do common hibernate functions, such as opening sessions
+ */
+public class HibernateUtility {
+	
+	private static SessionFactory sessionFactory = null;
+	
+	/**
+	 * Get the SessionFactory if it exists. If it does not, then 
+	 * create one and return it. 
+	 * 
+	 * @return The session factory. Returns null if there is an error creating the session factory
+	 */
+	public static SessionFactory getSessionFactory () 	{
+		
+		/*
+		 * Check if we already have a session factory. If we do not 
+		 * have one, then create it
+		 */
+		if(null == sessionFactory)
+		{
+
+			/*
+			 * We do not have a session factory. Create it.
+			 */
+			try
+			{
+				final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+				sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+			}
+			catch (HibernateException exception)
+			{
+				/*
+				 * Use a logger here
+				 */
+			}
+		}
+		
+		/*
+		 * Return the session factory. 
+		 * 
+		 * Note:
+		 * It the creation of the session factory fails, then null is returned.
+		 */
+		return sessionFactory;
+	}
+
+}
