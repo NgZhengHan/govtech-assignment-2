56c9913d10dde8f90fa6267a1af9110937d05866
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-14 20:46:16 +0800

    Added Hibernate mapping file for Household Added more variables to FamilyMember
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- src/ngzhenghan/govtech/assignment/entity/FamilyMember.java
+++ src/ngzhenghan/govtech/assignment/entity/FamilyMember.java
@@ -5,6 +5,10 @@ package ngzhenghan.govtech.assignment.entity;
 
 import java.util.Date;
 
+import ngzhenghan.govtech.assignment.entity.enums.Gender;
+import ngzhenghan.govtech.assignment.entity.enums.MaritalStatus;
+import ngzhenghan.govtech.assignment.entity.enums.OccupationType;
+
 /**
  * @author Ng Zheng Han
  * 
@@ -16,7 +20,14 @@ public class FamilyMember {
 	
 	private Long id = null;
 	private String name = UNDEFINED_STRING;
+	private Gender gender = Gender.UNDEFINED;
+	private Long genderOrdinal = null;
+	private MaritalStatus maritalStatus = MaritalStatus.UNDEFINED;
+	private Long maritalStatusOrdinal = null;
+	private FamilyMember spouse = null;
 	private Long spouseId = null;
+	private OccupationType occupationType = OccupationType.UNDEFINED;
+	private Long occupationTypeOrdinal = null;
 	private Double annualIncome = null;
 	private Date dateOfBirth = null;
 	
