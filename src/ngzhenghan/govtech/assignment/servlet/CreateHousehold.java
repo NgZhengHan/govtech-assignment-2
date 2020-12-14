1e30a80552ec78c815ef80b06f331342112af158
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-15 00:46:04 +0800

    Added javax.ws.rs-api-2.1.jar Fixed error in CreateHousehold.java
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- src/ngzhenghan/govtech/assignment/servlet/CreateHousehold.java
+++ src/ngzhenghan/govtech/assignment/servlet/CreateHousehold.java
@@ -3,11 +3,17 @@
  */
 package ngzhenghan.govtech.assignment.servlet;
 
+import javax.ws.rs.Path;
+
 /**
  * @author Ng Zheng Han
  *
  * Servlet to create a Household
  */
+
+@Path("/household")
 public class CreateHousehold {
+	
+	
 
 }
