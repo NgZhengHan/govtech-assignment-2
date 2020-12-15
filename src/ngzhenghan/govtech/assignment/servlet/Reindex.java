00ff4a8b9b738ce92da164cf5ee9c2e937c8a455
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-16 04:39:16 +0800

    Modified Reindex to return a response to indicate when reindex is done
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- src/ngzhenghan/govtech/assignment/servlet/Reindex.java
+++ src/ngzhenghan/govtech/assignment/servlet/Reindex.java
@@ -3,12 +3,16 @@
  */
 package ngzhenghan.govtech.assignment.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ngzhenghan.govtech.assignment.search.SearchManager;
+import ngzhenghan.govtech.assignment.utility.Utility;
 
 /**
  * @author Ng Zheng Han
@@ -27,6 +31,27 @@ public class Reindex extends HttpServlet 	{
 	protected void doGet (HttpServletRequest givenRequest, HttpServletResponse givenResponse) 	{
 
 		SearchManager.reindex();
+		
+		Utility.printDebugStatement("create success, printing response");
+		try(PrintWriter writer = givenResponse.getWriter();) 
+		{
+			Utility.printDebugStatement("setting content type to plain text");
+			givenResponse.setContentType("text/plain");
+			Utility.printDebugStatement("setting encoding type to utf-8");
+			givenResponse.setCharacterEncoding("UTF-8");
+			Utility.printDebugStatement("creating content");
+			writer.println("reindex: " + "completed");
+			Utility.printDebugStatement("flushing");
+			writer.flush();
+			Utility.printDebugStatement("flushed");
+		} 
+		catch (IOException e) 
+		{
+			/*
+			 * Use logger here
+			 */
+			Utility.printDebugStatement("error creating response");
+		}
 	}
 	
 }
