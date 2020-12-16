842af939c95a3217826ec12595a5c0ed798c5495
Ng Zheng Han <ng.zheng.han@gmail.com>
2020-12-16 21:00:52 +0800

    Added test method to test search by household
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

---
+++ database_backup/assignment_2_server_backup20201216_2.sql
@@ -0,0 +1,474 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2020-12-16 20:53:20
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md59b2ca9e1895be369a82ce7709e31cfa5';
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.0
+
+-- Started on 2020-12-16 20:53:20
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2020-12-16 20:53:21
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "assignment_2_database" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.0
+
+-- Started on 2020-12-16 20:53:21
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 2978 (class 1262 OID 16391)
+-- Name: assignment_2_database; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE assignment_2_database WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE assignment_2_database OWNER TO postgres;
+
+\connect assignment_2_database
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 205 (class 1259 OID 16434)
+-- Name: family_member_spouse_mapping_id_sequence; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.family_member_spouse_mapping_id_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.family_member_spouse_mapping_id_sequence OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 204 (class 1259 OID 16419)
+-- Name: family_member_spouse_mapping_table; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.family_member_spouse_mapping_table (
+    id bigint NOT NULL,
+    family_member_id_a bigint NOT NULL,
+    family_member_id_b bigint NOT NULL
+);
+
+
+ALTER TABLE public.family_member_spouse_mapping_table OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 16399)
+-- Name: family_members; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.family_members (
+    id bigint NOT NULL,
+    name character varying(255) NOT NULL,
+    gender_ordinal bigint NOT NULL,
+    marital_status_ordinal bigint NOT NULL,
+    occupation_type_ordinal bigint NOT NULL,
+    annual_income double precision,
+    date_of_birth date,
+    spouse_id bigint
+);
+
+
+ALTER TABLE public.family_members OWNER TO postgres;
+
+--
+-- TOC entry 201 (class 1259 OID 16397)
+-- Name: household_id_sequence; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.household_id_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.household_id_sequence OWNER TO postgres;
+
+--
+-- TOC entry 206 (class 1259 OID 16436)
+-- Name: household_members_id_sequence; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.household_members_id_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.household_members_id_sequence OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 16404)
+-- Name: household_members_mapping_table; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.household_members_mapping_table (
+    id bigint NOT NULL,
+    household_id bigint NOT NULL,
+    family_member_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.household_members_mapping_table OWNER TO postgres;
+
+--
+-- TOC entry 200 (class 1259 OID 16392)
+-- Name: households; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.households (
+    id bigint NOT NULL,
+    housing_type_ordinal bigint NOT NULL
+);
+
+
+ALTER TABLE public.households OWNER TO postgres;
+
+--
+-- TOC entry 2970 (class 0 OID 16419)
+-- Dependencies: 204
+-- Data for Name: family_member_spouse_mapping_table; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.family_member_spouse_mapping_table (id, family_member_id_a, family_member_id_b) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2968 (class 0 OID 16399)
+-- Dependencies: 202
+-- Data for Name: family_members; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.family_members (id, name, gender_ordinal, marital_status_ordinal, occupation_type_ordinal, annual_income, date_of_birth, spouse_id) FROM stdin;
+1	John Doe	2	2	4	150000	2020-12-15	\N
+2	John Doe	2	2	4	150000	2020-12-15	\N
+3	John Doe	2	2	4	150000	2020-12-16	\N
+4	John Doe	2	2	4	150000	2020-12-16	\N
+5	John Doe	2	2	4	150000	2020-12-16	\N
+6	John Doe	2	2	4	150000	2020-12-16	\N
+7	John Doe	2	2	4	150000	2020-12-16	\N
+\.
+
+
+--
+-- TOC entry 2969 (class 0 OID 16404)
+-- Dependencies: 203
+-- Data for Name: household_members_mapping_table; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.household_members_mapping_table (id, household_id, family_member_id) FROM stdin;
+22	4	1
+23	4	2
+24	4	3
+25	4	4
+\.
+
+
+--
+-- TOC entry 2966 (class 0 OID 16392)
+-- Dependencies: 200
+-- Data for Name: households; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.households (id, housing_type_ordinal) FROM stdin;
+5	4
+6	4
+7	4
+8	4
+9	4
+10	4
+11	4
+12	4
+13	4
+14	4
+15	4
+16	4
+17	4
+18	4
+19	4
+20	4
+21	4
+22	4
+23	4
+24	4
+25	4
+4	4
+26	4
+27	4
+28	4
+29	4
+30	3
+\.
+
+
+--
+-- TOC entry 2980 (class 0 OID 0)
+-- Dependencies: 205
+-- Name: family_member_spouse_mapping_id_sequence; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.family_member_spouse_mapping_id_sequence', 7, true);
+
+
+--
+-- TOC entry 2981 (class 0 OID 0)
+-- Dependencies: 201
+-- Name: household_id_sequence; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.household_id_sequence', 30, true);
+
+
+--
+-- TOC entry 2982 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: household_members_id_sequence; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.household_members_id_sequence', 45, true);
+
+
+--
+-- TOC entry 2828 (class 2606 OID 16423)
+-- Name: family_member_spouse_mapping_table family_member_spouse_mapping_table_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_member_spouse_mapping_table
+    ADD CONSTRAINT family_member_spouse_mapping_table_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2822 (class 2606 OID 16403)
+-- Name: family_members family_members_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_members
+    ADD CONSTRAINT family_members_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2824 (class 2606 OID 16408)
+-- Name: household_members_mapping_table household_members_mapping_table_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.household_members_mapping_table
+    ADD CONSTRAINT household_members_mapping_table_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2820 (class 2606 OID 16396)
+-- Name: households households_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.households
+    ADD CONSTRAINT households_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2826 (class 2606 OID 16443)
+-- Name: household_members_mapping_table unique_key_1; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.household_members_mapping_table
+    ADD CONSTRAINT unique_key_1 UNIQUE (household_id, family_member_id);
+
+
+--
+-- TOC entry 2830 (class 2606 OID 16450)
+-- Name: family_member_spouse_mapping_table unique_key_2; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_member_spouse_mapping_table
+    ADD CONSTRAINT unique_key_2 UNIQUE (family_member_id_a, family_member_id_b);
+
+
+--
+-- TOC entry 2833 (class 2606 OID 16414)
+-- Name: household_members_mapping_table family_member_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.household_members_mapping_table
+    ADD CONSTRAINT family_member_id FOREIGN KEY (family_member_id) REFERENCES public.family_members(id);
+
+
+--
+-- TOC entry 2834 (class 2606 OID 16424)
+-- Name: family_member_spouse_mapping_table family_member_id_a; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_member_spouse_mapping_table
+    ADD CONSTRAINT family_member_id_a FOREIGN KEY (family_member_id_a) REFERENCES public.family_members(id);
+
+
+--
+-- TOC entry 2835 (class 2606 OID 16429)
+-- Name: family_member_spouse_mapping_table family_member_id_b; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_member_spouse_mapping_table
+    ADD CONSTRAINT family_member_id_b FOREIGN KEY (family_member_id_b) REFERENCES public.family_members(id);
+
+
+--
+-- TOC entry 2832 (class 2606 OID 16409)
+-- Name: household_members_mapping_table household_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.household_members_mapping_table
+    ADD CONSTRAINT household_id FOREIGN KEY (household_id) REFERENCES public.households(id);
+
+
+--
+-- TOC entry 2831 (class 2606 OID 16444)
+-- Name: family_members spouse_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.family_members
+    ADD CONSTRAINT spouse_id FOREIGN KEY (spouse_id) REFERENCES public.family_members(id) NOT VALID;
+
+
+--
+-- TOC entry 2979 (class 0 OID 0)
+-- Dependencies: 2978
+-- Name: DATABASE assignment_2_database; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE CONNECT,TEMPORARY ON DATABASE assignment_2_database FROM PUBLIC;
+REVOKE ALL ON DATABASE assignment_2_database FROM postgres;
+GRANT ALL ON DATABASE assignment_2_database TO postgres WITH GRANT OPTION;
+
+
+-- Completed on 2020-12-16 20:53:21
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+\connect postgres
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.0
+
+-- Started on 2020-12-16 20:53:21
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2020-12-16 20:53:21
+
+--
+-- PostgreSQL database dump complete
+--
+
+-- Completed on 2020-12-16 20:53:21
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
--- src/ngzhenghan/govtech/assignment/test/TestSearchHousehold.java
+++ src/ngzhenghan/govtech/assignment/test/TestSearchHousehold.java
@@ -5,18 +5,13 @@ package ngzhenghan.govtech.assignment.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ngzhenghan.govtech.assignment.entity.HouseholdMemberMapping;
-import ngzhenghan.govtech.assignment.entity.manager.HouseholdMemberManager;
-import ngzhenghan.govtech.assignment.serialization.SerializationUtility;
+import ngzhenghan.govtech.assignment.entity.manager.HouseholdManager;
+import ngzhenghan.govtech.assignment.search.request.SearchHouseholdRequest;
 import ngzhenghan.govtech.assignment.utility.Utility;
 
 /**
@@ -25,47 +20,42 @@ import ngzhenghan.govtech.assignment.utility.Utility;
  * Helper class to test
  */
 
-@WebServlet(name = "TestAddMembers", urlPatterns = "/test/add-members")
-public class TestAddMembers extends HttpServlet 	{
+@WebServlet(name = "TestSearchHousehold", urlPatterns = "/test/search-household")
+public class TestSearchHousehold extends HttpServlet 	{
 
 	/**
 	 * Generated serial
 	 */
-	private static final long serialVersionUID = -1517669196217782136L;
+	private static final long serialVersionUID = 5647855293046084088L;
 
-	
 	@Override
 	protected void doGet (HttpServletRequest givenRequest, HttpServletResponse givenResponse) 	{
 
 		Utility.printDebugStatement("doGet");
+
+		SearchHouseholdRequest searchHouseholdRequest = new SearchHouseholdRequest();
 		
-		HouseholdMemberMapping householdMemberMapping = new HouseholdMemberMapping();
-		Map<Long, List<Long>> householdMembers = householdMemberMapping.getHouseholdIdsToFamilyMemberIds();
-		List<Long> familyMemberIds = new ArrayList<>();
-		familyMemberIds.add(Long.valueOf("1"));
-		familyMemberIds.add(Long.valueOf("2"));
-		familyMemberIds.add(Long.valueOf("3"));
-		familyMemberIds.add(Long.valueOf("4"));
-		householdMembers.put(Long.valueOf("4"), familyMemberIds);
+		searchHouseholdRequest.setWithMembersUnder(Double.valueOf("16"));
+		searchHouseholdRequest.setTotalIncome(Double.valueOf("150000"));
 		
-		Utility.buildAndPrintHttpPostForObject("http://localhost:8080/govtech-assignment-2/add-members", 
-												null,
-												householdMemberMapping);
+		Utility.buildAndPrintHttpPostForObject("http://localhost:8080/govtech-assignment-2/search/households", 
+												null, 
+												searchHouseholdRequest);
 		
 		/*
 		 * Use the entity manager to perform the operation
 		 */
-		Utility.printDebugStatement("try add");
-		Boolean result = HouseholdMemberManager.addHouseholdMembers(householdMemberMapping);
+		Utility.printDebugStatement("try search");
+		String result = HouseholdManager.searchByHousehold(searchHouseholdRequest);
 
 
 		if(null == result)
 		{
-			Utility.printDebugStatement("add failed");
+			Utility.printDebugStatement("search failed");
 		}
 		else
 		{
-			Utility.printDebugStatement("add success");
+			Utility.printDebugStatement("search success");
 		}
 		
 		/*
@@ -89,7 +79,7 @@ public class TestAddMembers extends HttpServlet 	{
 		}
 		else
 		{
-			Utility.printDebugStatement("add success, printing response");
+			Utility.printDebugStatement("get success, printing response ");
 			try(PrintWriter writer = givenResponse.getWriter();) 
 			{
 				Utility.printDebugStatement("setting content type to json");
@@ -99,9 +89,9 @@ public class TestAddMembers extends HttpServlet 	{
 				Utility.printDebugStatement("setting encoding type to utf-8");
 				givenResponse.setCharacterEncoding("UTF-8");
 				Utility.printDebugStatement("creating content");
-				writer.println("id of add entity: " + result.toString());
+				writer.println("search results: ");
 				Utility.printDebugStatement("creating content json");
-				writer.println("Details: " + SerializationUtility.toJson(householdMemberMapping));
+				writer.println("Details: " + result);
 				Utility.printDebugStatement("flushing");
 				writer.flush();
 				Utility.printDebugStatement("flushed");
@@ -115,4 +105,5 @@ public class TestAddMembers extends HttpServlet 	{
 			}
 		}
 	}
+	
 }
